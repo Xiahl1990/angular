983458943f2e5bc8f994a48e12910e0105349860
Xiahl1990 <971441973@qq.com>
2016-08-29 09:46:32 +0800

    中文注释核心指令的代码解释

--- modules/@angular/common/src/directives/core_directives.ts
+++ modules/@angular/common/src/directives/core_directives.ts
@@ -19,13 +19,16 @@ import {NgTemplateOutlet} from './ng_template_outlet';
 /**
  * A collection of Angular core directives that are likely to be used in each and every Angular
  * application.
+ * Angular核心指令集合都有可能被使用在每一个Angular应用中。
  *
  * This collection can be used to quickly enumerate all the built-in directives in the `directives`
  * property of the `@Component` annotation.
+ * 这个集合可以用来快速枚举“组件”注解中的“指令”属性中的所有内置指令。
  *
  * ### Example ([live demo](http://plnkr.co/edit/yakGwpCdUkg0qfzX5m8g?p=preview))
  *
  * Instead of writing:
+ * 而不是写下面这样：
  *
  * ```typescript
  * import {NgClass, NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
@@ -41,6 +44,7 @@ import {NgTemplateOutlet} from './ng_template_outlet';
  * }
  * ```
  * one could import all the core directives at once:
+ * 一个可以一次导入所有核心指令：
  *
  * ```typescript
  * import {CORE_DIRECTIVES} from '@angular/common';
